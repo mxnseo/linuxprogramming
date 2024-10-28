a10b132b4c1514db4553356054b74de89622de5c
mxnseo <5l8ct@naver.com>
2024-10-28 17:30:59 +0900

    리눅스 과제 업로드

--- chapter7-1/test1/mylib.hpp
+++ chapter7-1/test1/mylib.hpp
@@ -8,10 +8,9 @@ namespace MINSEO { // 네임 스페이스 설정
     class Complex { // 클래스 정의
         int real;  // 실수부
         int img;   // 허수부
-        bool noPrint; // sum 대입 시 생성자 출력 문구 방지
     public: // 접근 지정자
         // 생성자
-        Complex(int r = 0, int i = 0, bool noPrint = false);
+        Complex(int r = 0, int i = 0);
         // 복소수 출력 함수
         void show();
         // friend 선언으로 전역함수가 private 멤버 접근 가능하도록 함
@@ -19,4 +18,4 @@ namespace MINSEO { // 네임 스페이스 설정
     };
 }
 
-#endif // !_MYLIB_H
+#endif // !_MYLIB_HH
